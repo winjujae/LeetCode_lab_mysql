75c406cb729deb662cc9e3f5755938b7c2539307
ZB053 <136467253+winjujae@users.noreply.github.com>
2024-06-07 01:06:42 +0900

    Time: 1152 ms (100.00%), Space: 0B (100.00%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -5,5 +5,4 @@ from product p
 
 #select a.product_name, b.year, b.price
 #from Product a
-#right join Sales b
-#on a.product_id = b.product_id
+    #right join Sales b on a.product_id = b.product_id
