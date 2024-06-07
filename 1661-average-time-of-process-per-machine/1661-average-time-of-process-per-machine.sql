58acfee07be6a4a4e569e4a2685876a9cbe4894f
ZB053 <136467253+winjujae@users.noreply.github.com>
2024-06-08 03:10:16 +0900

    Time: 224 ms (99.12%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -8,3 +8,13 @@ from (
     group by machine_id, process_id
 ) as subquery
 group by machine_id
+
+#select a.machine_id, round(avg(subqueryA.timestamp - a.timestamp),3) as processing_time
+#from activity as a
+#    inner join (select * from activity
+#                    where activity_type = 'end'
+#                    group by machine_id, process_id
+#                ) as subqueryA
+#    on a.machine_id = subqueryA.machine_id
+#where a.activity_type = 'start'
+#group by machine_id
